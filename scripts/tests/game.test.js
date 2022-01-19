5bc0cd661ebc8fad33113f6351304d2e17bf2005
marinamarshall <marinamarshall2@gmail.com>
2022-01-19 06:54:26 +0000

    Add button ids to choices array and testing

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -15,14 +15,17 @@ beforeAll(() => {
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
-    })
+    });
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
-    })
+    });
     test("playerMoves key exists", () => {
         expect("playerMoves" in game).toBe(true);
-    })
+    });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
-    })
+    });
+    test("choices contains the correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
