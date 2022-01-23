58930ce4f3c3f12c796b3e9f7cc3f76482408a9d
marinamarshall <marinamarshall2@gmail.com>
2022-01-23 19:27:33 +0000

    Add addTurn function

--- scripts/game.js
+++ scripts/game.js
@@ -5,17 +5,30 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 }
 
+// function newGame() {
+//     clear currentGame
+//     clear playerMoves
+//     reset score to 0
+// }
+
 // reset the game score to zero, clear playerMoves array, clear currentGame array
 function newGame() {
     game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = {game, newGame, showScore};
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+module.exports = {game, newGame, showScore, addTurn};
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const {game, newGame, showScore} = require("../game");
+const {game, newGame, showScore, addTurn} = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -41,14 +41,23 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear the computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
+    // test("should clear the computer sequence array", () => {
+    //     expect(game.currentGame.length).toBe(0);
+    // });
+    test("should be one element in the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("should clear the playerMoves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
     test("should display zero for the element with the ID of score", () => {
         expect(document.getElementById("score").innerText).toBe(0);
+    });
+});
+
+describe("addTurn function should add to the sequence", () => {
+    test("addTurn function should add to the sequence", () => {
+        expect()
     });
 });
 
